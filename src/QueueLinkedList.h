27db5f9b20e5cc1bbd1cabf1196a2d0100daefa2
Adam Poper <64482829+AdamPoper@users.noreply.github.com>
2020-09-13 21:30:11 -0400

    Update QueueLinkedList.h

--- src/QueueLinkedList.h
+++ src/QueueLinkedList.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 #include <assert.h>
-
+// this is a custom Queue data structure implemented by linked list
 namespace ap
 {
 	template<typename type> class QueueLinkedList;
